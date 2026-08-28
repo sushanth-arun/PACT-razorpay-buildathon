4277abb0af416f92f1ab12198068247851a6a97f
sushanth-arun <sushantharun2006@gmail.com>
2026-08-29 00:45:09 +0530

    refactor: remove ClickSpark component and global canvas overlay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppSidebar } from "@/components/layout/AppSidebar";
 import { TopHeader } from "@/components/layout/TopHeader";
-import ClickSpark from "@/components/ClickSpark";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,25 +29,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-950 text-slate-100 flex min-h-screen relative overflow-x-hidden`}
       >
-        <ClickSpark
-          sparkColor="#ffffff"
-          sparkSize={10}
-          sparkRadius={15}
-          sparkCount={8}
-          duration={400}
-        >
-          <div className="flex min-h-screen w-full pl-64">
-            <AppSidebar />
-            <div className="flex-1 flex flex-col min-w-0">
-              <TopHeader />
-              <main className="flex-1 bg-slate-950/50">{children}</main>
-            </div>
+        <div className="flex min-h-screen w-full pl-64">
+          <AppSidebar />
+          <div className="flex-1 flex flex-col min-w-0">
+            <TopHeader />
+            <main className="flex-1 bg-slate-950/50">{children}</main>
           </div>
-        </ClickSpark>
+        </div>
       </body>
     </html>
   );
 }
+
 
 
 
